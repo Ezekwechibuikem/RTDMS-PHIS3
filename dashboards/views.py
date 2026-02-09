e7648edc0661a40391c15f591e50b71f2904c66d
Ezekwechibuikem <ezekwechibuikem@gmail.com>
2026-02-09 10:49:04 +0100

    created the dashboard app

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def dashboard(request):
-    return HttpResponse("Welcome to the Dashboard!")
+    return HttpResponse("Welcome to the Dashboard!, This is a placeholder view.")
 
 
