b8e8e249e7cd5d2a77511502c6a726502005ea1d
Ezekwechibuikem <ezekwechibuikem@gmail.com>
2026-02-09 10:39:45 +0100

    created url on account branch

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
+    # 'rest_framework',
+    # 'api',
     'accounts',
 ]
 
