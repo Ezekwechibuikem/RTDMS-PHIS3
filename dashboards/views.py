be33b66d09516d028099a1f74b9389f9859a54cf
Ezekwe Chibuikem <ezekwechibuikem@gmail.com>
2026-03-02 13:48:59 +0100

    branch issues

--- dashboards/views.py
+++ dashboards/views.py
@@ -5,11 +5,3 @@ from django.contrib.auth.decorators import login_required
 def dashboard(request):
     return render(request, 'dashboards/dashboard.html')
 
-
-@login_required
-def profile(request):
-    display_profile = request.user
-    context = {
-        'display_profile': display_profile
-    }
-    return render(request, 'dashboards/profile.html', context)
