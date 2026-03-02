d365ac6d29283fbec4387c7743960f153703353a
Ezekwe Chibuikem <ezekwechibuikem@gmail.com>
2026-03-02 13:40:16 +0100

    created the attendance branch

--- dashboards/views.py
+++ dashboards/views.py
@@ -6,3 +6,10 @@ def dashboard(request):
     return render(request, 'dashboards/dashboard.html')
 
 
+@login_required
+def profile(request):
+    display_profile = request.user
+    context = {
+        'display_profile': display_profile
+    }
+    return render(request, 'dashboards/profile.html', context)
